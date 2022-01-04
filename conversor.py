feb65ec4871b11c8148b73e4f93463d38fb665aa
Kevin Bejarano <kevincamilo1725@gmail.com>
2022-01-04 13:50:21 -0500

    usando funciones

--- conversor.py
+++ conversor.py
@@ -1,5 +1,22 @@
-pesos = float(input('Ingrese la cantidad de pesos'))
-dolar = 4080
-dolar= pesos /dolar
-dolar =round(dolar, 2)
-print('Usted tiene: $ ', dolar,' dolares')
+# print('Bienvenido al sistema de conversión nacional en Colombia')
+# opcion=float(input('ingrese la moneda que usara, 1 pesos, 2 dolares'))
+# if (opcion!=1 and opcion!=2):
+#     print('La opción seleccionada no es válida, por favor intente nuevamente')
+# elif(opcion==1):
+#     pesos = float(input('Ingrese la cantidad de pesos'))
+#     dolar = 4080
+#     dolar= pesos /dolar
+#     dolar =round(dolar, 2)
+#     print('Usted tiene: $ ', dolar,' dolares')
+# elif(opcion==2):
+#     dolares = float(input('Ingrese la cantidad de dolares'))
+#     dolares=round(4080*dolares,2)
+#     print('Usted tiene: ',dolares,' pesos')
+def saludo(nombre):
+    print('hola ',nombre, ' es un gusto conocerle')
+
+nombre=input('cual es su nombre')
+saludo(nombre)
+    
+    
+
